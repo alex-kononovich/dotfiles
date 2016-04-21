bb8f39521d91cc8554e73e55b46c01c4f34ed843
Alex K <flskif@gmail.com>
2016-04-21 17:49:48 +0300

    Make Emacs closer to my Vim setup
    - Evil-leader for leader bindings
    - Project management with Projectile
    - Search with Ag
    - File and buffer finder with Ido
    - Some most used keybindigs like <leader>d to kill buffer

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,6 +13,9 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; garbage collector threshold
+(setq gc-cons-threshold 20000000)
+
 ;; visual
 (set-frame-font "Menlo-15" nil t)
 (set-scroll-bar-mode nil)
@@ -30,11 +33,57 @@
 (set-frame-parameter nil 'fullscreen 'fullscreen)
 
 ;; git
-(use-package magit :ensure magit)
+(use-package magit
+  :config
+  (progn
+    (evil-leader/set-key
+      "gs" 'magit-status)))
 
 ;; indentation settings
 (setq-default indent-tabs-mode nil)
 (setq tab-width 2)
+
+;; 80 columns by default
+(setq-default fill-column 80)
+
+;; ag
+(use-package ag)
+
+;; projectile
+(use-package projectile
+  :config
+  (progn
+    (projectile-global-mode)
+    (evil-leader/set-key
+      "t" 'projectile-find-file
+      "f" 'projectile-ag
+      "pp" 'projectile-switch-project)))
+
+;; ido
+(ido-mode 1)
+(ido-everywhere 1)
+;; use ido in most places
+(use-package ido-ubiquitous :config (ido-ubiquitous-mode 1))
+;; fuzzy matching
+(use-package flx-ido :config (flx-ido-mode 1))
+;; vertical mode
+(use-package ido-vertical-mode
+  :config
+  (progn
+    (ido-vertical-mode 1)
+    ;; move with C-n and C-p
+    (setq ido-vertical-define-keys 'C-n-and-C-p-only)))
+
+;; evil-leader
+(use-package evil-leader
+  :config
+  (progn
+    (evil-leader/set-leader "<SPC>")
+    (evil-leader/set-key
+      "b" 'switch-to-buffer
+      "q" 'kill-buffer-and-window
+      "d" 'kill-this-buffer
+      "s" 'save-buffer)))
 
 ;; evil must go after all packages
 (use-package evil
@@ -42,4 +91,7 @@
   (progn
     (setq evil-want-C-u-scroll t)
     (setq evil-shift-width 2))
-  :config (evil-mode 1))
+  :config
+  (progn
+    (global-evil-leader-mode)
+    (evil-mode 1)))

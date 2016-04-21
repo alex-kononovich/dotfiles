4dd4c5d9399286ec6cb28bbe44b0606265b8b45e
Alex K <flskif@gmail.com>
2016-04-21 13:16:34 +0300

    Use 2 spaces per tab. Retab init.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -8,19 +8,19 @@
                          ("gnu" . "http://elpa.gnu.org/packages/")))
 
 (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 (require 'use-package)
 
 ;; visual
 (set-frame-font "Menlo-15" nil t)
 (set-scroll-bar-mode nil)
 (use-package solarized-theme
-	     :ensure solarized-theme
-	     :config
-	     (progn
-		(load-theme 'solarized-light t)
-		(setq x-underline-at-descent-line t)))
+  :ensure solarized-theme
+  :config
+  (progn
+    (load-theme 'solarized-light t)
+    (setq x-underline-at-descent-line t)))
 
 ;; disable startup screen
 (setq inhibit-startup-screen t)
@@ -32,8 +32,15 @@
 ;; git
 (use-package magit :ensure magit)
 
+;; indentation settings
+(setq-default indent-tabs-mode nil)
+(setq tab-width 2)
+
 ;; evil must go after all packages
 (use-package evil
-	     :ensure evil
-	     :init (setq evil-want-C-u-scroll t)
-	     :config (evil-mode 1))
+  :ensure evil
+  :init
+  (progn
+    (setq evil-want-C-u-scroll t)
+    (setq evil-shift-width 2))
+  :config (evil-mode 1))

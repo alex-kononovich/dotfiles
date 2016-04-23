ce5407c3775158dd132c6f6e4d724d7345220b99
Alex K <flskif@gmail.com>
2016-04-23 15:16:39 +0300

    Use ido-other-window to open vert/hor split

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -15,6 +15,8 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+(add-to-list 'load-path (expand-file-name "custom" user-emacs-directory))
+
 
 ;;
 ;; internals
@@ -48,6 +50,17 @@
 ;; M-x with ido
 (use-package smex :config (global-set-key (kbd "M-x") 'smex))
 
+(require 'ido-other-window)
+
+(defun my-ido-setup ()
+  ;; very convinient in case of typos
+  (define-key ido-completion-map (kbd "C-w") 'backward-kill-word)
+  ;; remap some ido-other-window
+  (define-key ido-completion-map (kbd "C-o") 'ido-invoke-in-other-window)
+  (define-key ido-completion-map (kbd "C-s") 'ido-invoke-in-vertical-split)
+  (define-key ido-completion-map (kbd "C-v") 'ido-invoke-in-horizontal-split))
+
+(add-hook 'ido-setup-hook 'my-ido-setup)
 
 ;;
 ;; visuals

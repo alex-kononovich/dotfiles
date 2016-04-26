e2959e7368f39acdd1732b638e118cd9b7a9f1ca
Alex K <flskif@gmail.com>
2016-04-26 14:47:54 +0300

    Fix problem with Tig

--- osx.sh
+++ osx.sh
@@ -5,3 +5,6 @@
 mkdir -p ~/Library/KeyBindings
 ln `dirname $0`/osx/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict
 
+# Fix problem with Tig - it gets suspended when C-y is pressed
+# https://github.com/jonas/tig/issues/214
+stty dsusp undef

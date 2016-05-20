5e44f01f691f5a0b119377476c99ff41b39a7429
Alex K <flskif@gmail.com>
2016-05-20 20:32:25 +0300

    Add flycheck-elm

--- emacs.d/custom/languages/my-elm.el
+++ emacs.d/custom/languages/my-elm.el
@@ -5,4 +5,10 @@
   (setq elm-tags-on-save t)
   (setq elm-format-on-save t))
 
+(use-package flycheck-elm
+  :commands (flycheck-elm-setup))
+
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
+
 (provide 'my-elm)

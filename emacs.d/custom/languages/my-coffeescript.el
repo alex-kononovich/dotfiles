488d16f495d50162c913242b297a5fc299fc5112
Alex K <flskif@gmail.com>
2016-08-18 13:47:46 +0300

    Setup indentation for coffeescript

--- emacs.d/custom/languages/my-coffeescript.el
+++ emacs.d/custom/languages/my-coffeescript.el
@@ -1,5 +1,7 @@
 (use-package coffee-mode
   :mode (("\\.cjsx$" . coffee-mode))
-  :commands (coffee-mode))
+  :commands (coffee-mode)
+  :config
+  (custom-set-variables '(coffee-tab-width 2)))
 
 (provide 'my-coffeescript)

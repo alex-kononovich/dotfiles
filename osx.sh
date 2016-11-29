fabe16097f3fa2f023c803d17c2163e1f83f1102
Alex K <flskif@gmail.com>
2016-11-29 17:17:36 +0300

    Save my Pomodoro timer

--- osx.sh
+++ osx.sh
@@ -8,3 +8,7 @@ ln `dirname $0`/osx/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBindi
 # Fix problem with Tig - it gets suspended when C-y is pressed
 # https://github.com/jonas/tig/issues/214
 stty dsusp undef
+
+# Apps
+
+brew cask install tomighty # Pomodoro timer

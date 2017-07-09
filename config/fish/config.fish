7321c9e1f67ae4ce07aff651e1933996f2125ea2
Alex K <flskif@gmail.com>
2017-07-09 22:04:07 +0300

    Change double quotes to single quotes in aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,26 +19,26 @@ set fish_user_paths \
 set -x MANPATH $MANPATH "$coreutils_path/libexec/gnuman"
 
 # ls
-alias ls="ls --color=auto"
-alias l="ls -lAh"
+alias ls='ls --color=auto'
+alias l='ls -lAh'
 
 # git
-alias gr="git recent"
-alias gf="git flow"
-alias gs="git status"
-alias gd="git diff"
-alias gdc="git diff --cached"
-alias ga="git add"
-alias gaa="git add --all"
-alias gc="git commit --verbose"
-alias gca="git commit --verbose --all"
-alias gc!="git commit --verbose --amend"
-alias gco="git checkout"
+alias gr='git recent'
+alias gf='git flow'
+alias gs='git status'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias ga='git add'
+alias gaa='git add --all'
+alias gc='git commit --verbose'
+alias gca='git commit --verbose --all'
+alias gc!='git commit --verbose --amend'
+alias gco='git checkout'
 
 # tmux
-alias ts="tmux new-session -s"
-alias tl="tmux list-sessions"
-alias ta="tmux attach-session -t"
+alias ts='tmux new-session -s'
+alias tl='tmux list-sessions'
+alias ta='tmux attach-session -t'
 
 # autojump
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish

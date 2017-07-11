02a9fabf2ae7124e2e41381ed47893507a88f4a0
Alex K <flskif@gmail.com>
2017-07-11 21:40:09 +0300

    Show diffrent states for git prompt
    Set indicator chars as documented in __fish_git_prompt.
    
    Chose to stick with git_prompt (instead of vcs_prompt) because I don't
    use svn or hg (which are also supported by vcs_prompt alongside git).

--- config/fish/functions/fish_right_prompt.fish
+++ config/fish/functions/fish_right_prompt.fish
@@ -1,3 +1,23 @@
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_char_stateseparator ''
+
+set -g __fish_git_prompt_char_cleanstate ''
+set -g __fish_git_prompt_char_dirtystate ' *'
+set -g __fish_git_prompt_char_stagedstate ' +'
+set -g __fish_git_prompt_char_invalidstate ' #'
+
+set -g __fish_git_prompt_char_untrackedfiles ' …'
+
+set -g __fish_git_prompt_char_upstream_equal ''
+set -g __fish_git_prompt_char_upstream_ahead ' ↑'
+set -g __fish_git_prompt_char_upstream_behind ' ↓'
+set -g __fish_git_prompt_char_upstream_diverged ' ⥄ '
+
+set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_color_branch normal
+set -g __fish_git_prompt_color_upstream yellow
+
 function fish_right_prompt
-  __fish_vcs_prompt
+  __fish_git_prompt '%s'
 end
+

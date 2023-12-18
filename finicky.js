386b8a05b563713fe95ba081780aa5cf59e0c758
Alex K <flskif@gmail.com>
2023-12-18 12:07:22 -0800

    Update finicky.js config

--- finicky.js
+++ finicky.js
@@ -35,6 +35,7 @@ module.exports = {
         /.*browserstack.com/,
         /.*launchdarkly.com/,
         /.*leapsome.com/,
+        /.*fathom.video/,
       ]),
       browser: "Google Chrome"
     },

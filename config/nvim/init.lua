5933a55f889deebfe54d55615964382f9e8565c8
Alex K <flskif@gmail.com>
2026-06-12 21:57:53 -0700

    Remove Opencode plugin

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -181,11 +181,6 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
     update_in_insert = false,
   })
 
--- Open Code
-vim.o.autoread = true -- auto-reload files changed by the agent
-vim.api.nvim_create_user_command("Opencode", function(opts)
-  require("opencode_send").send(opts)
-end, { range = true, nargs = "?", desc = "Send selection or filename to opencode along with a message" })
 
 require("lazy").setup({
   performance = {
